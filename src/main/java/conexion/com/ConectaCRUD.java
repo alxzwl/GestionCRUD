b4165ee03f5d65f3408469c7026b6e727dc20628
Pc <alxzwl@gmail.com>
2022-05-22 18:43:34 -0500

    :tada: Cargar de archivos de configuracion y librerias. Realizar la conexion

---
+++ src/main/java/conexion/com/ConectaCRUD.java
@@ -0,0 +1,56 @@
+package conexion.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConectaCRUD
+ */
+@WebServlet("/ConectaCRUD")
+public class ConectaCRUD extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConectaCRUD() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/gestionpedidoscrud?useSSL=false";
+		String usuario = "root";
+		String contra = "root";
+		String driver = "com.mysql.jdbc.Driver";
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			PrintWriter out = response.getWriter();
+			out.print("Nombre de la BBDD: "+jdbcUrl);
+			Class.forName(driver);
+			Connection miConexion = DriverManager.getConnection(jdbcUrl, usuario, contra);
+			out.print("Conectado");
+			miConexion.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

442c4bab6348cea5cb4e45d641e9679504c048fc
Pc <alxzwl@gmail.com>
2022-05-22 22:06:37 -0500

    :tada: Archivos basicos para la creacion de un CRUD

---
+++ src/main/java/entity/Cliente.java
@@ -0,0 +1,78 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+	@Column(name="nombre")
+	private String nombre;
+
+	@Column(name="apellidos")
+	private String apellidos;
+
+	@Column(name="email")
+	private String email;
+
+	public Cliente() {
+
+	}
+	
+	public Cliente(int id, String nombre, String apellidos, String email) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email + "]";
+	}
+	
+}
